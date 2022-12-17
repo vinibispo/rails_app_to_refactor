aee85006dbd20fe189283100ccb94e65358ac498
vinibispo <vini.bispo015@gmail.com>
2022-12-17 08:41:26 -0300

    feat(user): add RegisterUserService

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,34 +4,15 @@ class Users::RegistrationsController < ApplicationController
   def create
     user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
 
-    password = user_params[:password].to_s.strip
-    password_confirmation = user_params[:password_confirmation].to_s.strip
+    user_attributes = { name: user_params[:name], email: user_params[:email], password: user_params[:password], password_confirmation: user_params[:password_confirmation] }
 
-    errors = {}
-    errors[:password] = ["can't be blank"] if password.blank?
-    errors[:password_confirmation] = ["can't be blank"] if password_confirmation.blank?
+    status, json = RegisterUserService.new.call(user_attributes:)
 
-    if errors.present?
-      render_json(422, user: errors)
+    case status
+    when :password_err, :confirmation_err, :error
+      render_json(422, user: json)
     else
-      if password != password_confirmation
-        render_json(422, user: { password_confirmation: ["doesn't match password"] })
-      else
-        password_digest = Digest::SHA256.hexdigest(password)
-
-        user = User.new(
-          name: user_params[:name],
-          email: user_params[:email],
-          token: SecureRandom.uuid,
-          password_digest: password_digest
-        )
-
-        if user.save
-          render_json(201, user: user.as_json(only: [:id, :name, :token]))
-        else
-          render_json(422, user: user.errors.as_json)
-        end
-      end
+      render_json(201, user: json)
     end
   end
 end
