6850ed68877ecc936928a69422c3835e10b5628b
vinibispo <vini.bispo015@gmail.com>
2022-12-17 09:24:09 -0300

    feat(user): add pattern matching instead of simple case when

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,14 +4,15 @@ class Users::RegistrationsController < ApplicationController
   def create
     user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
 
-    user_attributes = { name: user_params[:name], email: user_params[:email], password: user_params[:password], password_confirmation: user_params[:password_confirmation] }
+    user_attributes = { name: user_params[:name], email: user_params[:email], password: user_params[:password],
+                        password_confirmation: user_params[:password_confirmation] }
 
     status, json = RegisterUserService.new.call(user_attributes:)
 
-    case status
-    when :password_err, :confirmation_err, :error
+    case [status, json]
+    in [:password_err, _] | [:confirmation_err, _] | [:error, _]
       render_json(422, user: json)
-    else
+    in [:ok, _]
       render_json(201, user: json)
     end
   end
