7fc66bbad564a6416d4225c16dcb260a401a7f75
vinibispo <vini.bispo015@gmail.com>
2022-12-18 15:14:35 -0300

    feat(todos): move TodosController to Todos::ItemController

---
+++ app/controllers/todos/item_controller.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+module Todos
+  class ItemController < ApplicationController
+    before_action :authenticate_user
+
+    BuildItem = ->(item) { Serializer.new(item).as_json }
+    private_constant :BuildItem
+
+    def index
+      todos = ::Todo::FilterItems.new.call(user_id: current_user.id, status: params[:status]&.strip&.downcase)
+
+      render_json(200, todos: todos.map(&BuildItem))
+    end
+
+    def create
+      todo_attributes = {
+        user_id: current_user.id,
+        title: todo_params[:title],
+        due_at: todo_params[:due_at]
+      }
+      status, todo = ::Todo::Create.new.call(todo_attributes:)
+
+      case [status, todo]
+      in [:ok, _] then render_json(201, todo: BuildItem[todo])
+      in [:error, _] then render_json(422, todo: BuildItem[todo])
+      end
+    end
+
+    def show
+      status, todo = ::Todo::Find.new.call(user_id: current_user.id, id: params[:id])
+      case [status, todo]
+      in [:ok, _] then render_json(200, todo: BuildItem[todo])
+      else render_json(404, todo: { id: 'not found' })
+      end
+    end
+
+    def destroy
+      status, todo = ::Todo::Delete.new.call(user_id: current_user.id, id: params[:id])
+
+      case [status, todo]
+      in [:ok, _] then render_json(200, todo: BuildItem[todo])
+      else render_json(404, todo: { id: 'not found' })
+      end
+    end
+
+    def update
+      conditions = {
+        id: params[:id],
+        user_id: current_user.id
+      }
+
+      attributes = {
+        title: todo_params[:title]
+      }
+      status, todo = ::Todo::Update.new.call(conditions:, attributes:)
+
+      case [status, todo]
+      in [:ok, _] then render_json(200, todo: BuildItem[todo])
+      in [:not_found, _] then render_json(404, todo: { id: 'not found' })
+      else render_json(422, todo: BuildItem[todo])
+      end
+    end
+
+    def complete
+      conditions = {
+        id: params[:id],
+        user_id: current_user.id
+      }
+
+      status, todo = ::Todo::Complete.new.call(conditions:)
+      case status
+      in :ok then render_json(200, todo: BuildItem[todo])
+      else render_json(404, todo: { id: 'not found' })
+      end
+    end
+
+    def uncomplete
+      conditions = {
+        id: params[:id],
+        user_id: current_user.id
+      }
+
+      status, todo = ::Todo::Uncomplete.new.call(conditions:)
+      case status
+      in :ok then render_json(200, todo: BuildItem[todo])
+      else render_json(404, todo: { id: 'not found' })
+      end
+    end
+
+    private
+
+    def todo_params
+      params.require(:todo).permit(:title, :due_at)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
     resources :registrations, only: :create
   end
 
-  resources :todos do
-    member do
-      put 'complete'
-      put 'uncomplete'
-    end
-  end
+  get '/todos/', to: 'todos/item#index', as: 'todos'
+  post '/todos/', to: 'todos/item#create'
+  get '/todos/:id', to: 'todos/item#show', as: 'todo'
+  put '/todos/:id', to: 'todos/item#update'
+  delete '/todos/:id', to: 'todos/item#destroy'
+  put '/todos/:id/complete', to: 'todos/item#complete', as: 'complete_todo'
+  put '/todos/:id/uncomplete', to: 'todos/item#uncomplete', as: 'uncomplete_todo'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
