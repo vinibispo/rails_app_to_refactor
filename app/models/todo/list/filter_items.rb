f6a0e3d58df3508bc8c0fb7a0f032ab589eb4f13
vinibispo <vini.bispo015@gmail.com>
2022-12-19 22:15:13 -0300

    feat(todo): add validation of user_id in Todo::List::AddItem

--- app/models/todo/list/filter_items.rb
+++ app/models/todo/list/filter_items.rb
@@ -1,6 +1,9 @@
 module Todo
   class List::FilterItems
     def call(user_id:, status:)
+      id = ID.new(user_id)
+      return [:error, id.validation_error] if id.invalid?
+
       todos = case status
               in 'overdue' then Item::Record.overdue
               in 'completed' then Item::Record.completed
@@ -8,7 +11,7 @@ module Todo
               else Item::Record.all
               end
 
-      todos.where(user_id:)
+      todos.where(user_id: id.value)
     end
   end
 end
