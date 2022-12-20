16c52cc09707695e59e46fa5076f5c53df3f9343
vinibispo <vini.bispo015@gmail.com>
2022-12-19 22:29:20 -0300

    feat(todo::item): add title validation in Todo::Item::Update

--- app/models/todo/item/update.rb
+++ app/models/todo/item/update.rb
@@ -2,6 +2,12 @@ module Todo::Item
   class Update
     def call(conditions:, attributes:)
       status, todo = Find.new.call(**conditions)
+
+      if attributes[:title].present?
+        title = ::Todo::Title.new(attributes[:title])
+        attributes[:title] = title.value
+      end
+
       case status
       in :ok
         return [:ok, todo] if todo.update(attributes)
