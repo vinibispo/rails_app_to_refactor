11bbdccc92f61435a48598cebe8b6fc38b18c3d2
vinibispo <vini.bispo015@gmail.com>
2022-12-19 22:23:54 -0300

    feat(todo::item): add id and user_id validation

--- app/models/todo/item/find.rb
+++ app/models/todo/item/find.rb
@@ -1,7 +1,12 @@
 module Todo::Item
   class Find
     def call(user_id:, id:)
-      todo = Record.find_by(user_id:, id:)
+      id = ID.new(id)
+      user_id = ID.new(user_id)
+
+      return [:not_found, nil] if id.invalid? || user_id.invalid?
+
+      todo = Record.find_by(user_id: user_id.value, id: id.value)
       status = todo.present? ? :ok : :not_found
       [status, todo]
     end
